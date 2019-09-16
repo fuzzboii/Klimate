8a39e1890df4662ca5c6d01532f51c31784634ff
RK-USN <226996@usn.no>
2019-09-16 16:48:24 +0200

    Lagt inn .JS, trengs til hamburger-meny

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function aapneHamburger() {
+    document.getElementById("TrengerIDHer").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function lukkHamburger() {
+    document.getElementById("TrengerIDHer").style.width = "0%";
+  }
