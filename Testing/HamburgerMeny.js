e51f2dfd8c07f8cf0ca72c1abd2353d4fec4975b
RK-USN <226996@usn.no>
2019-09-17 18:27:42 +0200

    Bruk av class for elementer som blir stylet på flere sider

--- Testing/HamburgerMeny.js
+++ Testing/HamburgerMeny.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function aapneHamburger() {
-  document.getElementById("hamburgerMeny").style.height = "100%";
+  document.getElementById("navMeny").style.height = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function lukkHamburger() {
-  document.getElementById("hamburgerMeny").style.height = "0%";
+  document.getElementById("navMeny").style.height = "0%";
 }
