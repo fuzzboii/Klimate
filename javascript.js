2c8c6aad2595168eced021a7afc6cb100a7058c2
RK-USN <226996@usn.no>
2019-09-23 15:30:13 +0200

    Lagt inn hamburgermeny i default

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
+  /* Open when someone clicks on the span element */
 function aapneHamburger() {
-    document.getElementById("TrengerIDHer").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function lukkHamburger() {
-    document.getElementById("TrengerIDHer").style.width = "0%";
-  }
+  document.getElementById("navMeny").style.height = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function lukkHamburger() {
+  document.getElementById("navMeny").style.height = "0%";
+}
