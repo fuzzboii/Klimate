0c7ac615a03ac0fe0aeca5ad2ffd984279d81774
RK-USN <226996@usn.no>
2019-10-24 18:43:49 +0200

    Tilbake til 100% i JavaScript til løsning er funnet for mobiler i landskap

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 /* Funksjonen som åpner gardinmenyen, bruker height for at den skal gå over hele siden */
 function aapneHamburger() {
-  document.getElementById("navMeny").style.height = "60%";
+  document.getElementById("navMeny").style.height = "100%";
 }
 
 /* Funksjonen som lukker gardinmenyen, bruker height for at innholdet ikke skal vises*/
