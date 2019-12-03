00ce01e00dbeca02faaace2df0bacba2c581ba53
RK-USN <226996@usn.no>
2019-12-03 09:52:50 +0100

    Riktig avatar i NavMeny på alle sider. Lagt inn sjekk på om bruker faktisk er innlogget på alle sider. Lagt til DOCTYPE til Konto og Konto_rediger, feil i CSS uten. Lagt til <html lang="no"> for å spesifisere hvilket språk siden har.

--- konto.php
+++ konto.php
@@ -1,8 +1,21 @@
 <?php
+session_start();
+// Ved adminside IF ($_SESSION['bruker'] and $_SESSION['brukertype'] == 1) {}
+if ($_SESSION['brukernavn']) {
+    // OK
+} else {
+    header("Location: default.php?error=1");
+}
 
+// Utlogging av bruker
+if(isset($_GET['loggUt']) && $_GET['loggUt'] == "true"){ 
+    session_destroy();
+    header("Location: default.php?utlogget=1");
+}
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="no">
 
     <head>
         <!-- Setter riktig charset -->
@@ -27,14 +40,11 @@
             <a class="bildeKontroll" href="javascript:void(0)" onclick="hamburgerMeny()" tabindex="3">
                 <img src="bilder/hamburgerIkon.svg" alt="Hamburger-menyen" class="hamburgerKnapp">
             </a>
-            <img src="bilder/brukerIkon.png" alt="Profilbilde" class="profil_navmeny">
-            <button onClick="loggUt()" id="backendLoggUt" tabindex="2">LOGG UT</button>
-            <script>
-                function loggUt() {
-                    sessionStorage.clear(); // Fungerer ikke
-                    window.location.replace("default.php");
-                }
-            </script>
+            <img src="bilder/profil.png" alt="Profilbilde" class="profil_navmeny">
+            <!-- Legger til en knapp for å logge ut når man er innlogget -->
+            <form method="GET" action="backend.php"> 
+                <button name="loggUt" id="backendLoggUt" formmethod="GET" tabindex="2" value="true">LOGG UT</button>
+            </form>
             <!-- Logoen øverst i venstre hjørne, denne leder alltid tilbake til default.php -->
             <a class="bildeKontroll" href="default.php" tabindex="1">
                 <img src="bilder/klimateNoText.png" alt="Klimate logo" class="Logo_navmeny">
