9ccb2b78b95da5bb712f9f43bc06e6d1056a97d2
RK-USN <226996@usn.no>
2020-02-08 00:06:53 +0100

    Tilbake på lokal server i development

--- innstillinger.php
+++ innstillinger.php
@@ -1,7 +1,6 @@
 <?php
 
 // Konstruktør for lokal server
-/*
 class mysqlPDO extends PDO {
     public function __construct() {
         $drv = 'mysql';
@@ -13,9 +12,9 @@ class mysqlPDO extends PDO {
         parent::__construct($dns,$usr,$pwd);
     }
 }
-*/
 
 // Konstruktør for skolens server
+/*
 class mysqlPDO extends PDO {
     public function __construct() {
         $drv = 'mysql';
@@ -27,6 +26,7 @@ class mysqlPDO extends PDO {
         parent::__construct($dsn,$usr,$pwd);
     }
 }
+*/
 
 // Konstruktør for Klimate.no
 /*
