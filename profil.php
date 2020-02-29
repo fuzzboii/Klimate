738e36c4558ed712e5b97c9c5ff6d472ca196893
RK-USN <226996@usn.no>
2020-02-29 21:02:54 +0100

    Fjernet footer og "Til topp", inkluderer footer.php

--- profil.php
+++ profil.php
@@ -628,19 +628,9 @@ $tabindex = 10;
                     <?php if($egen) {?>
                         <button onClick="location.href='profil.php?bruker=<?php echo $_SESSION['idbruker'] ?>&innstillinger'" name="redigerkonto" class="rediger_profil_knapp" tabindex=30>Rediger informasjon</button>
                     <?php } ?>
-            </main>
-
-             <?php } ?> <!-- Test på om brukeren har klikket på rediger -->
-            <!-- Knapp som vises når du har scrollet i vinduet, tar deg tilbake til toppen -->
-            <button onclick="tilbakeTilTopp()" id="toppKnapp" title="Toppen"><img src="bilder/pilopp.png" alt="Tilbake til toppen"></button>
-
-            <!-- Footer, epost er for øyeblikket på en catch-all, videresendes til RK -->
-            <footer>
-                <p class=footer_beskrivelse>&copy; Klimate <?php echo date("Y");?> | <a href="mailto:kontakt@klimate.no">Kontakt oss</a>
-                    <!-- Om brukeren ikke er administrator eller redaktør, vis link for søknad til å bli redaktør -->
-                    <?php if (isset($_SESSION['idbruker']) and $_SESSION['brukertype'] == "3") { ?> | <a href="soknad.php">Søknad om å bli redaktør</a><?php } ?>
-                </p>
-            </footer>
+                </main>
+            <?php } ?> <!-- Test på om brukeren har klikket på rediger -->
+            <?php include("inkluderes/footer.php") ?>
         </article>
     </body>
 
