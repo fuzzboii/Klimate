3bea2e5ae89b61d2beeb978676795b9c7464a0d4
RK-USN <226996@usn.no>
2020-02-29 21:02:51 +0100

    Fjernet footer og "Til topp", inkluderer footer.php

--- meldinger.php
+++ meldinger.php
@@ -725,17 +725,7 @@ if(isset($_POST['gjenopprettMelding'])) {
                 </main>
 
             <?php } ?>
-            
-            <!-- Knapp som vises når du har scrollet i vinduet, tar deg tilbake til toppen -->
-            <button onclick="tilbakeTilTopp()" id="toppKnapp" title="Toppen"><img src="bilder/pilopp.png" alt="Tilbake til toppen"></button>
-
-            <!-- Footer, epost er for øyeblikket på en catch-all, videresendes til RK -->
-            <footer>
-                <p class=footer_beskrivelse>&copy; Klimate 2020 | <a href="mailto:kontakt@klimate.no">Kontakt oss</a>
-                    <!-- Om brukeren ikke er administrator eller redaktør, vis link for søknad til å bli redaktør -->
-                    <?php if ($_SESSION['brukertype'] == "3") { ?> | <a href="soknad.php">Søknad om å bli redaktør</a><?php } ?>
-                </p>
-            </footer>
+            <?php include("inkluderes/footer.php") ?>
     </body>
 
     <!-- Denne siden er utviklet av Robin Kleppang og Glenn Petter Pettersen, siste gang endret 07.02.2020 -->
