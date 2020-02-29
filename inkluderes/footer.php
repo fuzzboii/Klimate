297c162cc9390f85c899b95aff22258b5b1f15d6
RK-USN <226996@usn.no>
2020-02-29 21:39:29 +0100

    Kommentar i footer

--- inkluderes/footer.php
+++ inkluderes/footer.php
@@ -1,4 +1,6 @@
-        <!-- Knapp som vises når du har scrollet i vinduet, tar deg tilbake til toppen -->
+<?php
+?> 
+<!-- Knapp som vises når du har scrollet i vinduet, tar deg tilbake til toppen -->
         <button onclick="tilbakeTilTopp()" id="toppKnapp" title="Toppen"><img src="bilder/pilopp.png" alt="Tilbake til toppen"></button>
 
         <!-- Footer, epost er for øyeblikket på en catch-all, videresendes til RK -->
@@ -8,3 +10,7 @@
                 <?php if (isset($_SESSION['idbruker']) and $_SESSION['brukertype'] == "3") { ?> | <a href="soknad.php">Søknad om å bli redaktør</a><?php } ?>
             </p>
         </footer>
+<?php
+// Denne siden er utviklet av Robin Kleppang, siste gang endret 29.02.2020
+// Denne siden er kontrollert av Robin Kleppang, siste gang 29.02.2020
+?>
