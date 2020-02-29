071ddf0a7070065aba8d0e70948cd2cfbde610a4
RK-USN <226996@usn.no>
2020-02-29 21:02:37 +0100

    Fjernet footer og "Til topp", inkluderer footer.php

--- backend.php
+++ backend.php
@@ -95,17 +95,7 @@ $sisteArrangement = $stmtArrangement->fetch(PDO::FETCH_ASSOC);
                     <a href="#">Trykk her for å lese videre</a>
                 </article>
             </main>
-
-            <!-- Knapp som vises når du har scrollet i vinduet, tar deg tilbake til toppen -->
-            <button onclick="tilbakeTilTopp()" id="toppKnapp" title="Toppen"><img src="bilder/pilopp.png" alt="Tilbake til toppen"></button>
-
-            <!-- Footer, epost er for øyeblikket på en catch-all, videresendes til RK -->
-            <footer>
-                <p class=footer_beskrivelse>&copy; Klimate <?php echo date("Y");?> | <a href="mailto:kontakt@klimate.no">Kontakt oss</a>
-                    <!-- Om brukeren ikke er administrator eller redaktør, vis link for søknad til å bli redaktør -->
-                    <?php if ($_SESSION['brukertype'] == "3") { ?> | <a href="soknad.php">Søknad om å bli redaktør</a><?php } ?>
-                </p>
-            </footer>
+            <?php include("inkluderes/footer.php") ?>
         </article>   
     </body>
 
