eafb2c6fe5c250bb8c07809997650f5a1ba6b3fe
RK-USN <226996@usn.no>
2020-03-18 16:00:58 +0100

    Lagt utviklet av i riktig område

--- rapport.php
+++ rapport.php
@@ -23,36 +23,37 @@ if (!isset($_SESSION['idbruker'])) {
 <!DOCTYPE html>
 <html lang="no">
 
-    <head>
-        <!-- Setter riktig charset -->
-        <meta charset="UTF-8">
-        <!-- Legger til viewport -->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Setter tittelen på prosjektet -->
-        <title>Rapport</title>
-        <!-- Henter inn ekstern stylesheet -->
-        <link rel="stylesheet" type="text/css" href="stylesheet.css">
-        <!-- Henter inn favicon, bildet som dukker opp i fanene i nettleseren -->
-        <link rel='icon' href='bilder/favicon.png' type='image/x-icon'>
-        <!-- Henter inn JavaScript -->
-        <script language="JavaScript" src="javascript.js"> </script>
-    </head>
+<head>
+    <!-- Setter riktig charset -->
+    <meta charset="UTF-8">
+    <!-- Legger til viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Setter tittelen på prosjektet -->
+    <title>Rapport</title>
+    <!-- Henter inn ekstern stylesheet -->
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <!-- Henter inn favicon, bildet som dukker opp i fanene i nettleseren -->
+    <link rel='icon' href='bilder/favicon.png' type='image/x-icon'>
+    <!-- Henter inn JavaScript -->
+    <script language="JavaScript" src="javascript.js"> </script>
+</head>
 
-    <body id="rapport_body" onclick="lukkMelding('mldFEIL_boks')">
-        <?php include("inkluderes/navmeny.php") ?>
+<body id="rapport_body" onclick="lukkMelding('mldFEIL_boks')">
+    <?php include("inkluderes/navmeny.php") ?>
 
-        <!-- For å kunne lukke hamburgermenyen ved å kun trykke på et sted i vinduet må lukkHamburgerMeny() funksjonen ligge i deler av HTML-koden -->
-        <!-- Kan ikke legge denne direkte i body -->
-        <header onclick="lukkHamburgerMeny()">
-            <!-- Overskrift på siden -->
-            <h1>Rapport</h1>
-        </header>
-        <main onclick="lukkHamburgerMeny()">
-            <?php echo($_POST['rapport']) ?>
-        </main>
-        <?php include("inkluderes/footer.php") ?>
-    </body>
+    <!-- For å kunne lukke hamburgermenyen ved å kun trykke på et sted i vinduet må lukkHamburgerMeny() funksjonen ligge i deler av HTML-koden -->
+    <!-- Kan ikke legge denne direkte i body -->
+    <header onclick="lukkHamburgerMeny()">
+        <!-- Overskrift på siden -->
+        <h1>Rapport</h1>
+    </header>
+    <main onclick="lukkHamburgerMeny()">
+        <?php echo($_POST['rapport']) ?>
+    </main>
+    <?php include("inkluderes/footer.php") ?>
+</body>
+
+<!-- Denne siden er utviklet av Robin Kleppang, siste gang endret 06.03.2020 -->
+<!-- Denne siden er kontrollert av , siste gang 06.03.2020 -->
+
 </html>
-
-    <!-- Denne siden er utviklet av Robin Kleppang, siste gang endret 06.03.2020 -->
-    <!-- Denne siden er kontrollert av , siste gang 06.03.2020 -->
