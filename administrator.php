c3ce312a2baf8b5c0ccf64fbb633974c4027f324
RK-USN <226996@usn.no>
2020-03-19 07:46:35 +0100

    Enda en if test

--- administrator.php
+++ administrator.php
@@ -359,7 +359,7 @@ if(isset($_POST['slettregel'])) {
                         </figure>
                         <p id="admin_brukernavn"><?php echo($brukerinfo['brukernavn']) ?></p>
                         <?php if($harFornavn) {echo("<p>Navn: " . $brukerinfo['fnavn']);} if($harEtternavn) {echo(" " . $brukerinfo['enavn']);} if(!$harFornavn && !$harEtternavn) {echo("<p id='admin_ikkeoppgitt'>Navn: Ikke oppgitt");} ?></p>
-                        <?php if($harFornavn) {echo("<p>Epost: " . $brukerinfo['epost']);} else {echo("<p id='admin_ikkeoppgitt'>Epost: Ikke oppgitt");} ?></p>
+                        <?php if($harEpost) {echo("<p>Epost: " . $brukerinfo['epost']);} else {echo("<p id='admin_ikkeoppgitt'>Epost: Ikke oppgitt");} ?></p>
                         <?php if($harTlf) {echo("<p>Telefon: " . $brukerinfo['telefonnummer']);} else {echo("<p id='admin_ikkeoppgitt'>Telefon: Ikke oppgitt");} ?></p>
                     </section>
                     <section id="admin_handlinger">
