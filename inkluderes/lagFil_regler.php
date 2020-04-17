ce2775391566a1a0d9f25d4f66ca27f289fa650e
RK-USN <226996@usn.no>
2020-04-17 10:26:48 +0200

    Byttet plassering på main og nav i oppretting av html

--- inkluderes/lagFil_regler.php
+++ inkluderes/lagFil_regler.php
@@ -18,11 +18,11 @@ if(!file_exists("./regler.html") && isset($_SESSION['idbruker'])) {
                         "\n\t\t<title>Regler</title>" .
                     "\n\t</head>");
     //Body og main start-tag
-    fwrite($handle,"\n\t<body>\n\t\t<main id='regler_innhold' onclick='lukkHamburgerMeny()'>");
+    fwrite($handle,"\n\t<body>");
 
     //Navmeny-bar og hamburgermeny
-    fwrite($handle,"\n\t\t\t<nav class='navTop'>\n\t\t\t\t<a class='bildeKontroll' href='javascript:void(0)' onclick='hamburgerMeny()' tabindex='6'>" .
-                    "\n\t\t\t\t\t<img src='bilder/hamburgerIkon.svg' alt='Hamburger-menyen' class='hamburgerKnapp'>\n\t\t\t\t</a>");
+    fwrite($handle,"\n\t\t<nav class='navTop'>\n\t\t\t<a class='bildeKontroll' href='javascript:void(0)' onclick='hamburgerMeny()' tabindex='6'>" .
+                    "\n\t\t\t\t<img src='bilder/hamburgerIkon.svg' alt='Hamburger-menyen' class='hamburgerKnapp'>\n\t\t\t</a>");
 
     // Henter bilde fra database utifra brukerid
     $hentBildeQ = "select hvor from bilder, brukerbilde where brukerbilde.bruker = " . $_SESSION['idbruker'] . " and brukerbilde.bilde = bilder.idbilder";
@@ -33,81 +33,81 @@ if(!file_exists("./regler.html") && isset($_SESSION['idbruker'])) {
     // Test'er på ulike brukere
     if ($antallBilderFunnet != 0) {
         // Hvis vi finner et bilde til bruker viser vi det 
-        fwrite($handle, "\n\t\t\t\t<a class='bildeKontroll' href='javascript:void(0)' onClick=location.href='profil.php?bruker=" . $_SESSION['idbruker'] . "' tabindex='5'>");
+        fwrite($handle, "\n\t\t\t<a class='bildeKontroll' href='javascript:void(0)' onClick=location.href='profil.php?bruker=" . $_SESSION['idbruker'] . "' tabindex='5'>");
         $testPaa = $bilde['hvor'];
         // Tester på om filen faktisk finnes
         if(file_exists("$lagringsplass/$testPaa")) {   
             if(file_exists("$lagringsplass/" . "thumb_" . $testPaa)) {
                 if ($_SESSION['brukertype'] == 2) {
                     // Setter redaktør border "Grønn" 
-                fwrite($handle,"\n\t\t\t\t\t<img src='bilder/opplastet/thumb_" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny' style='border: 2px solid green;'>");
+                fwrite($handle,"\n\t\t\t\t<img src='bilder/opplastet/thumb_" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny' style='border: 2px solid green;'>");
                 } else if ($_SESSION['brukertype'] == 1) {
                     // Setter administrator border "Rød" 
-                fwrite($handle,"\n\t\t\t\t\t<img src='bilder/opplastet/thumb_" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny' style='border: 2px solid red;'>");
+                fwrite($handle,"\n\t\t\t\t<img src='bilder/opplastet/thumb_" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny' style='border: 2px solid red;'>");
                 } else if ($_SESSION['brukertype'] == 3) {
                     // Setter vanlig profil bilde  
-                fwrite($handle,"\n\t\t\t\t\t<img src='bilder/opplastet/thumb_" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny'>"); 
+                fwrite($handle,"\n\t\t\t\t<img src='bilder/opplastet/thumb_" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny'>"); 
                 }
             } else { 
                 if ($_SESSION['brukertype'] == 2) {
                     // Setter redaktør border "Grønn" 
-                    fwrite($handle,"\n\t\t\t\t\t<img src='bilder/opplastet/" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny' style='border: 2px solid green;'>");
+                    fwrite($handle,"\n\t\t\t\t<img src='bilder/opplastet/" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny' style='border: 2px solid green;'>");
                 } else if ($_SESSION['brukertype'] == 1) {
                     // Setter administrator border "Rød" 
-                    fwrite($handle,"\n\t\t\t\t\t<img src='bilder/opplastet/" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny' style='border: 2px solid red;'>");
+                    fwrite($handle,"\n\t\t\t\t<img src='bilder/opplastet/" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny' style='border: 2px solid red;'>");
                 } else if ($_SESSION['brukertype'] == 3) {
                     // Setter vanlig profil bilde  
-                    fwrite($handle,"\n\t\t\t\t\t<img src='bilder/opplastet/" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny'>"); 
+                    fwrite($handle,"\n\t\t\t\t<img src='bilder/opplastet/" . $bilde['hvor'] . "' alt='Profilbilde'  class='profil_navmeny'>"); 
                 }
             }
         } else { 
             // Om filen ikke ble funnet, vis standard profilbilde
             if ($_SESSION['brukertype'] == 2) {
                 // Setter redaktør border "Grønn" 
-                fwrite($handle,"\n\t\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny' style='border: 2px solid green;'>");
+                fwrite($handle,"\n\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny' style='border: 2px solid green;'>");
             } else if ($_SESSION['brukertype'] == 1) {
                 // Setter administrator border "Rød" 
-                fwrite($handle,"\n\t\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny' style='border: 2px solid red;'>"); 
+                fwrite($handle,"\n\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny' style='border: 2px solid red;'>"); 
             } else if ($_SESSION['brukertype'] != 1 || 2) {
                 // Setter vanlig profil bilde 
-                fwrite($handle,"\n\t\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny'>");}
+                fwrite($handle,"\n\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny'>");}
         } 
-        fwrite($handle,"\n\t\t\t\t</a>");
+        fwrite($handle,"\n\t\t\t</a>");
     } else { 
-        fwrite($handle, "\n\t\t\t\t<a class='bildeKontroll' href='javascript:void(0)' onClick=location.href='profil.php?bruker=" . $_SESSION['idbruker'] . "' tabindex='5'>");
+        fwrite($handle, "\n\t\t\t<a class='bildeKontroll' href='javascript:void(0)' onClick=location.href='profil.php?bruker=" . $_SESSION['idbruker'] . "' tabindex='5'>");
         if ($_SESSION['brukertype'] == 2) {
             // Setter redaktør border "Grønn"
-            fwrite($handle,"\n\t\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny' style='border: 2px solid green;'>");
+            fwrite($handle,"\n\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny' style='border: 2px solid green;'>");
 
         } else if ($_SESSION['brukertype'] == 1) {
             // Setter administrator border "Rød" 
-            fwrite($handle,"\n\t\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny' style='border: 2px solid red;'>");
+            fwrite($handle,"\n\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny' style='border: 2px solid red;'>");
 
         } else if ($_SESSION['brukertype'] != 1 || 2) {
             // Setter vanlig profil bilde
-            fwrite($handle,"\n\t\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny'>");
+            fwrite($handle,"\n\t\t\t\t<img src='bilder/profil.png' alt='Profilbilde' class='profil_navmeny'>");
         } 
-        fwrite($handle,"\n\t\t\t\t</a>");
+        fwrite($handle,"\n\t\t\t</a>");
     }
     //Logg ut knapp
-    fwrite($handle,"\n\t\t\t\t<form id='loggUtKnappForm' method='POST' action='default.php'>" . 
-                        "\n\t\t\t\t\t<button name='loggUt' id='registrerKnapp' tabindex='4'>LOGG UT</button>" . 
-                    "\n\t\t\t\t</form>");
+    fwrite($handle,"\n\t\t\t<form id='loggUtKnappForm' method='POST' action='default.php'>" . 
+                        "\n\t\t\t\t<button name='loggUt' id='registrerKnapp' tabindex='4'>LOGG UT</button>" . 
+                    "\n\t\t\t</form>");
 
-    fwrite($handle, "\n\t\t\t\t<form id='sokForm_regler' action='sok.php'>" . 
-                        "\n\t\t\t\t\t<input id='sokBtn_navmeny' type='submit' value='' tabindex='3'>" .
-                        "\n\t\t\t\t\t<input id='sokInp_navmeny' type='text' name='artTittel' placeholder='Søk på artikkel' tabindex='2'>" . 
-                    "\n\t\t\t\t</form>" . 
+    fwrite($handle, "\n\t\t\t<form id='sokForm_regler' action='sok.php'>" . 
+                        "\n\t\t\t\t<input id='sokBtn_navmeny' type='submit' value='' tabindex='3'>" .
+                        "\n\t\t\t\t<input id='sokInp_navmeny' type='text' name='artTittel' placeholder='Søk på artikkel' tabindex='2'>" . 
+                    "\n\t\t\t</form>" . 
 
-                    "\n\t\t\t\t<a href='javascript:void(0)' onClick=location.href='sok.php' tabindex='-1'>" . 
-                        "\n\t\t\t\t\t<img src='bilder/sokIkon.png' alt='Søkeikon' class='sok_navmeny' tabindex='2'>" . 
-                    "\n\t\t\t\t</a>");
+                    "\n\t\t\t<a href='javascript:void(0)' onClick=location.href='sok.php' tabindex='-1'>" . 
+                        "\n\t\t\t\t<img src='bilder/sokIkon.png' alt='Søkeikon' class='sok_navmeny' tabindex='2'>" . 
+                    "\n\t\t\t</a>");
 
     //Klimate-logo'en
-    fwrite($handle,"\n\t\t\t\t<a href='default.php' tabindex='1'>" . 
-                        "\n\t\t\t\t\t<img class='Logo_navmeny' src='bilder/klimateNoText.png' alt='Klimate logo'>" . 
-                    "\n\t\t\t\t</a>" .
-                    "\n\t\t\t</nav>");
+    fwrite($handle,"\n\t\t\t<a href='default.php' tabindex='1'>" . 
+                        "\n\t\t\t\t<img class='Logo_navmeny' src='bilder/klimateNoText.png' alt='Klimate logo'>" . 
+                    "\n\t\t\t</a>" .
+                    "\n\t\t</nav>\n\t\t<main id='regler_innhold' onclick='lukkHamburgerMeny()'>");
 
                     
     //Rullegardin, tester på om brukeren er admin
