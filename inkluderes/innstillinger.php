4ed68076a995472fa396909e651ef919cec89f9c
RK-USN <226996@usn.no>
2020-06-04 17:18:29 +0200

    Utviklet av i innstillinger, venter med å bytte ut kobling

--- inkluderes/innstillinger.php
+++ inkluderes/innstillinger.php
@@ -123,8 +123,6 @@ if(!isset($default_melding) || substr($default_melding, 0, 10) != "Systemfeil") 
 }
 
 
-
-
-// Denne siden er utviklet av Robin Kleppang, siste gang endret 14.04.2020
+// Denne siden er utviklet av Robin Kleppang, siste gang endret 04.06.2020
 // Denne siden er kontrollert av Glenn Petter Pettersen & Robin Kleppang, siste gang 04.06.2020
 ?>
