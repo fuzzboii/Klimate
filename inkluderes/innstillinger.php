743bf42631773a48709392785fae56a59ac02576
RK-USN <226996@usn.no>
2020-06-05 10:57:29 +0200

    Utviklet av

--- inkluderes/innstillinger.php
+++ inkluderes/innstillinger.php
@@ -91,6 +91,6 @@ if(!isset($default_melding) || substr($default_melding, 0, 10) != "Systemfeil") 
 }
 
 
-// Denne siden er utviklet av Robin Kleppang, siste gang endret 04.06.2020
-// Denne siden er kontrollert av Glenn Petter Pettersen & Robin Kleppang, siste gang 04.06.2020
+// Denne siden er utviklet av Robin Kleppang, siste gang endret 05.06.2020
+// Denne siden er kontrollert av Glenn Petter Pettersen & Robin Kleppang, siste gang 05.06.2020
 ?>
